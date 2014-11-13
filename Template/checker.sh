4d91e3f35b4f8ded1238a3e52564f1a85181768a
yuzhou627 <yuzhou627@gmail.com>
2014-11-13 16:51:53 +0800

    checker.sh

---
+++ Template/checker.sh
@@ -0,0 +1,17 @@
+while true; do
+./gen>tmp.in #出数据
+./std<tmp.in>std.out #正确（暴力）程序
+./my<tmp.in>my.out #被测程序
+if diff my.out std.out; then #比较两个输出文件
+echo "AC" #结果相同显示AC
+else
+echo "WA" #结果不同显示WA，并退出
+#cmp std.out my.out
+exit 0
+fi #if的结束标志,与C语言相反，0为真
+done #while的结束标志
+
+#BY Lernece Ray 2014-09-15
+#在终端下，进入当前目录，输入"sh ./cheker.sh",（其中checker.sh为当前shell脚本名） '#'表示单行注释
+#diff在两文件相同时返回空串
+
