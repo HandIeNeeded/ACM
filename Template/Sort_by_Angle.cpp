ec419f2c3589634cf667d5dfeeeac6237f22328f
yuzhou627 <yuzhou627@gmail.com>
2014-12-12 01:36:29 +0800

    Sort_by_angle

---
+++ Template/Sort_by_Angle.cpp
@@ -0,0 +1,23 @@
+struct Point {
+	long long x, y;
+	bool flag;
+
+	Point (long long a = 0, long long b = 0) : x(a), y(b) {}
+
+	int dim() const {
+		return x < 0 || x == 0 && y < 0;
+	}
+};
+
+long long det(const Point &a, const Point &b) {
+	return a.x * b.y - a.y * b.x;
+}
+
+bool byAng(const Point &a, const Point &b) {
+	if (a.dim() == b.dim()) {
+		return det(a, b) < 0;
+	} 
+	else {
+		return a.dim() < b.dim();
+	}
+}
