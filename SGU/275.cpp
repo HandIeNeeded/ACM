b4ee729fd3c8cfaff34d357c00ae5dfd2f994ac8
yuzhou627 <yuzhou627@gmail.com>
2015-02-09 21:12:23 +0800

    To xor or not to xor

---
+++ SGU/275.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define REP(i, a) for (int i = 0; i < (int) (a); i ++)
+#define REPP(i, a, b) for (int i = (int) (a); i <= (int) (b); i ++)
+#define MST(a, b) memset(a, b, sizeof(a))
+
+using namespace std;
+
+const int N = 105;
+long long a[N];
+
+void Guass(int n, int &now) {
+	now = 1;
+	REP(i, 60) {
+		if (now > n) break;
+		long long bit = 1LL << (59 - i);
+		bool flag = 0;
+		REPP(j, now, n) {
+			if (a[j] & bit) {
+				swap(a[j], a[now]);
+				flag = 1;
+				now++;
+				break;
+			}
+		}
+		if(flag) {
+			REPP(j, 1, n) {
+				if (j != now - 1 && (a[j] & bit)) {
+					a[j] ^= a[now - 1];
+				}
+			}
+		}
+		//cout << i << endl;
+		//REPP(j, 1, n) {
+		//	cout << a[j] << ' ';
+		//}
+		//cout << "*****************************" << endl;
+	}
+}
+
+int main() {
+	ios :: sync_with_stdio(0);
+	int n;
+	cin >> n;
+	REPP(i, 1, n) {
+		cin >> a[i];
+	}
+	int now;
+	Guass(n, now);
+	long long ans = 0;
+	REPP(i, 1, now) ans ^= a[i];
+	cout << ans << endl;
+	
+	return 0;
+}
