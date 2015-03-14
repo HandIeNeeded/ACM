1938cc1bb95a3bac1e2ec309299f0e888d44eef1
yuzhou627 <yuzhou627@gmail.com>
2015-03-14 11:16:36 +0800

    Treap_2 Update

--- Template/Treap_2.cpp
+++ Template/Treap_2.cpp
@@ -1,5 +1,3 @@
-const int N = 2e5 + 5;
-
 struct Node{
 	int value, size;
 	Node *l, *r;
@@ -8,7 +6,17 @@ struct Node{
 		size = l->size + 1 + r->size;
 		return this;
 	}
-}*root, *null;
+}bar[N], *foo, *rt[N], *null;
+
+void init() {
+	foo = null = bar;
+	null->l = null->r = null;
+	foo++;
+}
+
+Node* New_Node(int x) {
+	return new(foo++) (Node) {x, 1, null, null};
+}
 
 bool gen(int a, int b) {
 	return rand() % (a + b) < a;
@@ -42,4 +50,56 @@ PNN split(Node *u, int s) {
 		u->r = res.first;
 		return {u->update(), res.second};
 	}
+}
+
+void show(Node *u) {
+	if (u == null) return ;
+	else {
+		show(u->l);
+		printf("value: %d\n", u->value);
+		show(u->r);
+	}
+}
+
+pair<int, bool> find(Node *u, int x) {
+	if (u == null) return {0, 0};
+	if (u->value == x) {
+		return {u->l->size + 1, 1};
+	}
+	else if (u->value < x) {
+		pair<int, bool> res = find(u->r, x);
+		res.first += u->l->size + 1;
+		return res;
+	}
+	else {
+		return find(u->l, x);
+	}
+}
+
+void insert(Node *a, Node *&u) {
+	pair<int, bool> ans = find(u, a->value);
+	if (!ans.second) {
+		PNN res = split(u, ans.first);
+		u = a;
+		u = merge(res.first, u);
+		u = merge(u, res.second);
+	}
+}
+
+void dfs(Node *a, Node *&b) {
+	if (a == null) return ;
+	else {
+		dfs(a->l, b);
+		dfs(a->r, b);
+		a->l = a->r = null;
+		insert(a->update(), b);
+	}
+}
+
+Node* combine(Node *a, Node *b) {
+	if (a->size > b->size) {
+		swap(a, b);
+	}
+	dfs(a, b);
+	return b;
 }
