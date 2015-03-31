8b4810299c252a18643aa188f4b057f307628355
yuzhou627 <yuzhou627@gmail.com>
2015-03-31 13:19:11 +0800

    Update

--- AsZ/2015.03.15/L.cpp
+++ AsZ/2015.03.15/L.cpp
@@ -22,16 +22,31 @@
 
 using namespace std;
 
-const int N = 305;
+const int N = 605;
 const int INF = 0x3f3f3f3f;
-int dp[N][N], Left[N], Right[N];
+int dp[N][N], n;
+vector<int> num;
 
 struct Enemy{
 	int x, y, h;
 }q[N];
 
-bool cmp(Enemy a, Enemy b) {
-	return a.x < b.x;
+int solve(int x, int y) {
+	if (x > y) return 0;
+	if (dp[x][y] != INF) return dp[x][y];
+	int mx = -1;
+	REPP(i, 1, n) {
+		if (q[i].x >= num[x] && q[i].y <= num[y] && q[i].h > mx) {
+			mx = q[i].h;
+		}
+	}
+	if (mx < 0) return dp[x][y] = 0;
+	else {
+		REPP(i, x, y) {
+			dp[x][y] = min(dp[x][y], solve(x, i - 1) + mx + solve(i + 1, y));
+		}
+		return dp[x][y];
+	}
 }
 
 int main() {
@@ -39,45 +54,22 @@ int main() {
 	int t;
 	cin >> t;
 	while (t--) {
-		int n;
 		cin >> n;
+		num.clear();
 		REPP(i, 1, n) {
 			int x, y, h;
 			cin >> x >> y >> h;
 			q[i] = {x, y, h};
+			num.push_back(x), num.push_back(y);
 		}
-		sort(q + 1, q + n + 1, cmp);
-		REPP(i, 1, n) {
-			Left[i] = Right[i] = i;
-			int &j = Left[i];
-			while (j > 0 && q[j].y >= q[i].x && q[j].h <= q[i].h) j--;
-			int &k = Right[i];
-			while (j <= n && q[k].x == q[i].x && q[k].h <= q[i].h) k++;
-		}
-		REPP(i, 1, n) {
-			cout << i << ' ' << Left[i] << ' ' << Right[i] << endl;
-		}
-		REPP(i, 1, n) dp[i][i] = q[i].h;
-		REPP(i, 1, n - 1) {
-			if (q[i].y >= q[i + 1].x) {
-				dp[i][i + 1] = max(dp[i][i], dp[i + 1][i + 1]);
-			}
-			else {
-				dp[i][i + 1] = dp[i][i] + dp[i + 1][i + 1];
+		sort(num.begin(), num.end());
+		int m = unique(num.begin(), num.end()) - num.begin();
+		REP(i, m) {
+			REP(j, m) {
+				dp[i][j] = INF;
 			}
 		}
-		REPP(k, 2, n - 1) {
-			REPP(i, 1, n) {
-				if (i + k <= n) {
-					int j = i + k;
-					dp[i][j] = INF;
-					REPP(mid, i, j) {
-						dp[i][j] = min(dp[i][j], dp[i][Left[i]] + q[mid].h + dp[Right[i]][j]);
-					}
-				}
-			}
-		}
-		cout << dp[1][n] << endl;
+		cout << solve(0, m - 1) << endl;
 	}
 	
 	return 0;
