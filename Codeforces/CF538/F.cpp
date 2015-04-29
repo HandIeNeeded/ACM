0ea1508eae3a55ba0e3e9d49723fe85d424abdaf
yuzhou627 <yuzhou627@gmail.com>
2015-04-30 03:12:35 +0800

    Update

--- Codeforces/CF538/F.cpp
+++ Codeforces/CF538/F.cpp
@@ -22,9 +22,53 @@
 
 using namespace std;
 
+const int N = 2e5 + 5;
+int val[N], ans[N];
+int cnt[N], n;
+
+void bfs(int size) {
+	queue<int> q;
+	q.push(1);
+	int now = 2;
+	while (q.size()) {
+		if (now > n) break;
+		int x = q.front(); q.pop();
+		REP(i, size) {
+			if (val[now] < val[x]) {
+				ans[size]++;
+			}
+			if (now <= n) q.push(now++);
+			if (now > n) break;
+		}
+	}
+}
+
 int main() {
 	ios :: sync_with_stdio(0);
-
+	cin >> n;
+	REPP(i, 1, n) cin >> val[i];
+	int K = int(sqrt(n)) + 1;
+	REPP(i, 1, K) bfs(i);
+	REPP(i, 1, K) {
+		REPP(j, 1, n) {
+			cnt[j] = cnt[j - 1] + (val[j] < val[i]);
+		}
+		if (i == 1) {
+			REPP(j, K + 1, n - 1) ans[j] += cnt[j + 1] - cnt[1];
+		}
+		else {
+			REPP(j, K + 1, n - 1) {
+				if ((j + 1) + (i - 2) * j < n) {
+					int up = min(j + 1 + (i - 1) * j, n);
+					ans[j] += cnt[up] - cnt[(j + 1) + (i - 2) * j];
+				}
+				else break;
+			}
+		}
+	}
+	REPP(i, 1, n - 1) {
+		cout << ans[i] << endl;
+	}
 	return 0;
 }
 
