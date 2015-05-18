1251a0c8a616319c79974ee31643a428c4664146
yuzhou627 <yuzhou627@gmail.com>
2015-05-19 06:33:24 +0800

    Update

--- Template/Closest_Pair.cpp
+++ Template/Closest_Pair.cpp
@@ -1,97 +1,40 @@
-//LL version
-struct po{
-	LL x, y;
-	po (LL a = 0, LL b = 0) : x(a), y(b) {}
-}p[N];
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Attention  sort(p, p + n, cmpxy) first!
-
-inline bool cmpxy(const po &a, const po &b) { //initailly  sort all points using cmpxy
-	if (a.x == b.x) return a.y < b.y;
-	return a.x < b.x;
-}
-
-inline bool cmpy(const int &a, const int &b) {return p[a].y < p[b].y;}
-
-LL sqr(LL x) {return x * x;}
-
-//return (dis * dis)
-
-LL dis(const int &i, const int &j) { return sqr(p[i].x - p[j].x) + sqr(p[i].y - p[j].y);} //square of dis
-
-int tmp[N];
-
-LL Closest_Pair(int left, int right) { 
-	LL d = LIM; // square of ans
-	if (left == right) return d;
-	if (left + 1 == right) return dis(left, right);
-	int mid = (left + right) >> 1;
-	LL  dl = Closest_Pair(left, mid);
-	LL dr = Closest_Pair(mid + 1, right);
-	d = min(dl, dr);
-	int cnt = 0;
-	REPP(i, left, right) {
-		if (sqr(p[mid].x - p[i].x) <= d) { //square of x
-			tmp[cnt++] = i;
-		} 
-	}
-	sort(tmp, tmp + cnt, cmpy);
-	REP(i, cnt) {
-		for (int j = i + 1; j < cnt && sqr(p[tmp[j]].y - p[tmp[i]].y) < d; ++j) { //square of y and attention !!!!!!!!!!!!!!!!!!!! tmp[i] not i
-			LL dd = dis(tmp[i], tmp[j]); // Attention tmp[i] not i !!!!!!!!!!!!!!!!!!!!!!!!! 
-			if (d > dd) d = dd;
-		}
-	}
-	return d;
-}
-
-//square of dis 
-// dis need to change dis function and sqr part
-
 //double version
-struct po{
-	double x, y;
-	po (double a = 0, double b = 0) : x(a), y(b) {} 
+struct point{
+    double x, y;
 }p[N];
-
-inline bool cmpxy(const po &a, const po &b) {
-	if (a.x != b.x) return a.x < b.x;
-	return a.y < b.y;
+//sort all points using cmp at first
+inline bool cmp(const point &a, const point &b) { 
+    if (a.x == b.x) return a.y < b.y;
+    return a.x < b.x;
 }
-
-inline bool cmpy(const int &i, const int &j) {return p[i].y < p[j].y;} 
-
-double sqr(double x) {return x * x;}
-
-double dis(const int &i, const int &j) {return sqrt(sqr(p[i].x - p[j].x) + sqr(p[i].y - p[j].y));} 
+inline bool cmpy(const int &a, const int &b) {return p[a].y < p[b].y;}
+inline double sqr(double x) {return x * x;}
+inline double dis(const int &i, const int &j) {
+    return sqrt(sqr(p[i].x - p[j].x) + sqr(p[i].y - p[j].y));
+}//square of dis
 
 int tmp[N];
-
-double Closest_Pair(int left, int right) {
-	double d = 1e100;
-	if (left == right) return d;
-	if (left + 1 == right) {
-		return dis(left, right);
-	}
-	int mid = (left + right) >> 1;
-	double dl = Closest_Pair(left, mid);
-	double dr = Closest_Pair(mid + 1, right);
-	int cnt = 0;
-	d = min(dl, dr);
-	REPP(i, left, right) {
-		if (abs(p[i].x - p[mid].x) <= d) {
-			tmp[cnt++] = i;
-		}
-	} 
-	sort(tmp, tmp + cnt, cmpy);
-	REP(i, cnt) {
-		for (int j = i + 1; j < cnt && p[tmp[j]].y - p[tmp[i]].y < d; ++j) { // !!!!!!!!!!!!!!!!!!!! tmp[i] not i
-			double dd = dis(tmp[i], tmp[j]); // !!!!!!!!!!!!!!!!!
-			if (d > dd) d = dd;
-		}
-	}
-	return d;
+//return (dis * dis)
+double Closest_Pair(int left, int right) { 
+    double d = INF; // square of ans
+    if (left == right) return d;
+    if (left + 1 == right) return dis(left, right);
+    int mid = (left + right) >> 1;
+    double  dl = Closest_Pair(left, mid);
+    double dr = Closest_Pair(mid + 1, right);
+    d = min(dl, dr);
+    int cnt = 0;
+    REPP(i, left, right) {
+        if (abs(p[mid].x - p[i].x) <= d) {
+            tmp[cnt++] = i;
+        } 
+    }
+    sort(tmp, tmp + cnt, cmpy);
+    REP(i, cnt) {
+        for (int j = i + 1; j < cnt && p[tmp[j]].y - p[tmp[i]].y < d; ++j) {
+            double dd = dis(tmp[i], tmp[j]); //tmp[i] not i
+            if (d > dd) d = dd;
+        }
+    }
+    return d;
 }
-// call sort(p, p + n, cmpxy) first !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
