8dd827c17763d0e58850e9e4ab4eeb94bed91a67
yuzhou627 <yuzhou627@gmail.com>
2015-06-17 21:16:44 +0800

    SGU_355

---
+++ SGU/355.cc
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <ctime>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+
+#define REP(i, a) REPP(i, 0, (a) - 1)
+#define REPP(i, a, b) for (int i = int(a); i <= int(b); i++)
+#define MST(a, b) memset(a, b, sizeof(a))
+
+using namespace std;
+
+const int N = 1e4 + 5;
+int p[N], cnt[N], vis[N], tot;
+
+void prime() {
+	for (int i = 2; i < N; i++) {
+		if (!vis[i]) p[tot++] = i, cnt[i] = 1;
+		for (int j = 0; j < tot && i * p[j] < N; j++) {
+			vis[i * p[j]] = p[j];
+			cnt[i * p[j]] = cnt[i] + 1;
+			if (i % p[j] == 0) break;
+		}
+	}
+}
+
+int main() {
+    ios :: sync_with_stdio(0);
+	prime();
+	int n;
+	cin >> n;
+	int ans = 0;
+	REPP(i, 1, n) ans = max(ans, cnt[i] + 1);
+	cout << ans << endl;
+	REPP(i, 1, n) {
+		cout << cnt[i] + 1 << " \n"[i == n];
+	}
+
+    return 0;
+}
+
