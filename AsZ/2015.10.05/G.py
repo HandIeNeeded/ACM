96dc7d3e6bdb40a50593b6f1a4748724876ccd2c
yuzhou627 <yuzhou627@gmail.com>
2015-10-21 12:50:31 +0800

    update G.py

--- AsZ/2015.10.05/G.py
+++ AsZ/2015.10.05/G.py
@@ -25,26 +25,27 @@ def solve(a, b):
     for i in xrange(3, n - 1):
         dp[i] = A[i] * dp[2] + B[i]
     key = dp[k]
+    return key
 
-    if a == 1:
-        dp[b], dp[n] = Decimal(1), Decimal(0)
-        A[0], B[0] = Decimal(0), dp[n]
-        A[1], B[1] = Decimal(1), Decimal(0)
-        for i in xrange(1, n - 1):
-            A[i + 1] = (A[i] - A[i - 1] * q[i]) / p[i]
-            B[i + 1] = (B[i] - B[i - 1] * q[i]) / p[i]
-        dp[1] = (dp[n - 1] - B[n - 1]) / A[n - 1]
-    else :
-        dp[b], dp[n] = Decimal(1), Decimal(0)
-        A[n], B[n] = Decimal(0), dp[n]
-        A[n - 1], B[n - 1] = Decimal(1), Decimal(0)
-        for i in xrange(n - 1, 1, -1):
-            A[i - 1] = (A[i] - A[i + 1] * p[i]) / q[i]
-            B[i - 1] = (B[i] - B[i + 1] * p[i]) / q[i]
-        dp[n - 1] = (dp[1] - B[1]) / A[1]
-    return key * dp[a]
+key = solve(1, n - 1)
 
 ans = Decimal(0)
-ans += solve(1, n - 1)
-ans += solve(n - 1, 1)
+dp[n - 1], dp[n] = Decimal(1), Decimal(0)
+A[0], B[0] = Decimal(0), dp[n]
+A[1], B[1] = Decimal(1), Decimal(0)
+for i in xrange(1, n - 1):
+    A[i + 1] = (A[i] - A[i - 1] * q[i]) / p[i]
+    B[i + 1] = (B[i] - B[i - 1] * q[i]) / p[i]
+dp[1] = (dp[n - 1] - B[n - 1]) / A[n - 1]
+ans += key * dp[1]
+
+dp[1], dp[n] = Decimal(1), Decimal(0)
+A[n], B[n] = Decimal(0), dp[n]
+A[n - 1], B[n - 1] = Decimal(1), Decimal(0)
+for i in xrange(n - 1, 1, -1):
+    A[i - 1] = (A[i] - A[i + 1] * p[i]) / q[i]
+    B[i - 1] = (B[i] - B[i + 1] * p[i]) / q[i]
+dp[n - 1] = (dp[1] - B[1]) / A[1]
+ans += (Decimal(1) - key) * dp[n - 1]
+
 print float(ans)
